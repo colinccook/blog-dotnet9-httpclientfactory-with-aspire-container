fdfddedfc12206c996b5a0ec9f3f00231d46132f
Colin C Cook <colinccook@gmail.com>
2025-07-10 06:37:11 +0100

    temporarily remove expectations

--- ColinCCook.AppHost.IntegrationTests/FooTests.cs
+++ ColinCCook.AppHost.IntegrationTests/FooTests.cs
@@ -7,9 +7,8 @@ namespace ColinCCook.AppHost.IntegrationTests;
 
 public class FooTests
 {
-    [TestCase(200, HttpStatusCode.OK)]
-    [TestCase(404, HttpStatusCode.BadRequest)]
-    public async Task ThirdPartyApiCall_Should_ReturnCorrectStatusCode(int barStatusCode, HttpStatusCode fooStatusCode)
+    [TestCase]
+    public async Task ReturnsBadRequest_When_BarServiceCallIsNotSuccessful()
     {
         // Arrange
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ColinCCook_AppHost>();
@@ -21,49 +20,74 @@ public class FooTests
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
         await app.StartAsync();
 
-        // MockService expectation
-        var httpClientMockServer = app.CreateHttpClient("servicename", "endpointname");
-        await resourceNotificationService.WaitForResourceAsync("servicename", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
-        var expectation = new
-        {
-            httpRequest = new
-            {
-                method = "GET",
-                path = "/bar"
-            },
-            httpResponse = new
-            {
-                statusCode = barStatusCode
-            }
-        };
-
-        var json = JsonSerializer.Serialize(expectation);
-        var content = new StringContent(json, Encoding.UTF8, "application/json");
-        await httpClientMockServer.PutAsync("/mockserver/expectation", content);
-
         // Act
         var httpClient = app.CreateHttpClient("webapi");
         await resourceNotificationService.WaitForResourceAsync("webapi", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
         var response = await httpClient.GetAsync("/foo");
 
         // Assert
-        Assert.That(response.StatusCode, Is.EqualTo(fooStatusCode));
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+    }
 
-        // Verify MockService expectation
-        var verification = new
-        {
-            httpRequest = new
-            {
-                method = "GET",
-                path = "/bar"
-            }
-        };
-        var verificationJson = JsonSerializer.Serialize(verification);
-        var verificationContent = new StringContent(verificationJson, Encoding.UTF8, "application/json");
-        var verificationResponse = await httpClientMockServer.PutAsync("/mockserver/verify", verificationContent);
-        Assert.That(verificationResponse.StatusCode, Is.EqualTo(HttpStatusCode.Accepted));
+    [TestCase]
+    public async Task ReturnsOK_When_BarServiceCallIsSuccessful()
+    {
+        Assert.Inconclusive("Test not yet implemented. Need to set the expectation within mockserver to return a successful response");
 
-        await httpClientMockServer.PutAsync("/mockserver/clear?type=EXPECTATIONS", null);
+        //     // Arrange
+        //     var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ColinCCook_AppHost>();
+        //     appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        //     {
+        //         clientBuilder.AddStandardResilienceHandler();
+        //     });
+        //     await using var app = await appHost.BuildAsync();
+        //     var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        //     await app.StartAsync();
+
+        //     // MockService expectation
+        //     var httpClientMockServer = app.CreateHttpClient("servicename", "endpointname");
+        //     await resourceNotificationService.WaitForResourceAsync("servicename", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        //     var expectation = new
+        //     {
+        //         httpRequest = new
+        //         {
+        //             method = "GET",
+        //             path = "/bar"
+        //         },
+        //         httpResponse = new
+        //         {
+        //             statusCode = barStatusCode
+        //         }
+        //     };
+
+        //     var json = JsonSerializer.Serialize(expectation);
+        //     var content = new StringContent(json, Encoding.UTF8, "application/json");
+        //     await httpClientMockServer.PutAsync("/mockserver/expectation", content);
+
+        //     // Act
+        //     var httpClient = app.CreateHttpClient("webapi");
+        //     await resourceNotificationService.WaitForResourceAsync("webapi", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        //     var response = await httpClient.GetAsync("/foo");
+
+        //     // Assert
+        //     Assert.That(response.StatusCode, Is.EqualTo(fooStatusCode));
+
+        //     // Verify MockService expectation
+        //     var verification = new
+        //     {
+        //         httpRequest = new
+        //         {
+        //             method = "GET",
+        //             path = "/bar"
+        //         }
+        //     };
+        //     var verificationJson = JsonSerializer.Serialize(verification);
+        //     var verificationContent = new StringContent(verificationJson, Encoding.UTF8, "application/json");
+        //     var verificationResponse = await httpClientMockServer.PutAsync("/mockserver/verify", verificationContent);
+        //     Assert.That(verificationResponse.StatusCode, Is.EqualTo(HttpStatusCode.Accepted));
+
+        //     await httpClientMockServer.PutAsync("/mockserver/clear?type=EXPECTATIONS", null);
+        // }
     }
 }
